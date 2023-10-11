8c043a6121543e01d7be944d789adeb240d3c19c
TatianaMaksakova <tatzianar@gmail.com>
2023-10-12 02:57:19 +0300

    add6 add class Hero update: update start_game function

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import pygame
 import sys
 
+def start_game():
+    pygame.init()
+    screen = pygame.display.set_mode((1000,800))
+    pygame.display.set_caption("SpaceX by mordovina")
+
+    flag = True
+    while flag:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        pygame.display.flip()
+
+
+start_game()
---
+++ super hero.py
@@ -0,0 +1,5 @@
+import pygame
+
+class SuperHero():
+    def __init__(self, image):
+        self.image = pygame.image.load()
