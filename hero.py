fefa4a453bc15e890ce28371c06d317791516b36
TatianaMaksakova <tatzianar@gmail.com>
2023-10-19 16:15:43 +0300

    update hero

--- hero.py
+++ hero.py
@@ -1,5 +1,23 @@
 import pygame
 
-class SuperHero():
-    def __init__(self, image):
-        self.image = pygame.image.load(images/hero.png)
+class MainCharacter():
+    def __init__(self, screen):
+        self.screen = screen
+        self.image = pygame.image.load("images/hero.png")
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+        self.move_right = False
+        self.move_left = False
+
+    def output(self):
+        self.screen.blit(self.image, self.rect)
+
+    def moving(self):
+        if self.move_right and self.rect.right < self.screen_rect.right:
+            self.rect.centerx += 1
+        elif self.move_left and self.rect.left > 0:
+            self.rect.centerx -= 1
+
+
--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import pygame
 import sys
-
-import pygame
-import sys
+from hero import MainCharacter
 
 def start_game():
     pygame.init()
-    screen = pygame.display.set_mode((1000,800))
-    pygame.display.set_caption("SpaceX by mordovina")
+    screen = pygame.display.set_mode((1000, 800))
+    pygame.display.set_caption("mordovina")
+    maincharacter = MainCharacter(screen)
 
     flag = True
     while flag:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    maincharacter.move_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_a:
+                    maincharacter.move_right = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    maincharacter.move_right = True
+
+
+
+        maincharacter.output()
         pygame.display.flip()
 
 
