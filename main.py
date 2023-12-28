ee508261e581394c7beee33fe3c26a9207c1fac6
xxxadaria <147159601+xxxadaria@users.noreply.github.com>
2023-12-28 12:08:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,61 +3,32 @@ import sys
 from main_character import MainCharacter
 from pygame.sprite import Group
 
-
+#основная функ ДЛЯ ОПИСАНИЯ игры
 def start_game():
     pygame.init()
     screen = pygame.display.set_mode((1000, 800))
     pygame.display.set_caption("mordovina")
+    
 
+#объекты классов
     maincharacter = MainCharacter(screen)
-    enemys = Group()
     bullets = Group()
-    controls.create_army(screen, enemys)
+    enemys = Group()
+    controls.create_army(screen,enemys)
 
     flag = True
     while flag:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_d:
-                    maincharacter.rect.centerx += 1
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_d:
-                    maincharacter.move_right = True
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_d:
-                    maincharacter.move_right = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                    maincharacter.move_right -= 1
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                    maincharacter.move_left = True
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_a:
-                    maincharacter.move_left = False
+        controls.events(screen, hero, bullets)
+        hero.moving_hero(screen)
 
+        controls.update(screen, hero, enemys, bullets)
+        controls.update_bullets(screen, bullets, enemys)
+        controls.update_enemys(hero,screen, bullets, enemys)
+        
 
-        maincharacter.output()
-        pygame.display.flip()
-        maincharacter.moving(screen)
+start_game()
 
-        screen.fill(0)
-        for bullet in bullets.sprites():
-            bullet.draw_bullet()
-
-
-        controls.update(screen, enemys, maincharacter, bullets)
-        controls.update_bullets(screen, enemys, bullets)
-
-
-
-        maincharacter.output()
-        pygame.display.flip()
-
-        bullets.update()
-        for bullet
+        
 
 
 
